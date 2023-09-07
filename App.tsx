ca837b99daa217fe7a0757e8f53c2e25c57320b4
Aakash <aakash@192.168.1.147>
2023-09-07 21:09:03 +0530

    boilerplate commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, SafeAreaView} from 'react-native';
+
+function App() {
+ 
+  return (
+    <SafeAreaView className="border border-gray-300">
+      <View className="px-3 py-5">
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Text>Hello World</Text>
+        <Text className="text-blue-500 text-xl px-5 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum dicta nihil unde eos
+          enim iste reiciendis temporibus officia quae consequuntur, aliquid quos recusandae ipsa
+          aperiam, similique nisi quasi nobis repellendus fuga optio. Accusamus?
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cum dicta nihil unde eos
+          enim iste reiciendis temporibus officia quae consequuntur, aliquid quos recusandae ipsa
+          aperiam, similique nisi quasi nobis repellendus fuga optio. Accusamus?
+        </Text>
+        
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
